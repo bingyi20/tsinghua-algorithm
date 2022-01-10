b22d112313b26234ea65bfe32256a7ee8be0e6a6
bingyi20 <yibangyu@gmail.com>
2022-01-10 11:03:55 +0800

    fix: 修改stack sort文件名

--- stack-sort/index.js
+++ stack-sort/index.js
@@ -12,6 +12,31 @@ function stackSort(R) {
     return S
 }
 
+// version 2 有瑕疵
+// 插入排序 O(I + n) 其中I是逆序对的个数，插入排序是输入敏感型算法
+function stackSort2(R) {
+    const S = []
+    if(!R.length) return S
+    let top = null
+    while(R.length || top !== null) {
+        if(top === null) {
+            top = R.pop()
+        }
+        // 空会被命中多少次
+        if(S.length == 0 || S[S.length - 1] <= top) {
+            S.push(top)
+            top = null
+        }else{
+            // push, pop操作会被命中多少次
+            R.push(S.pop())
+        }
+    }
+    return S
+}
+
 let R = [1,3,2,5,5,9,8,6,43,21,35]
 
-console.log(stackSort(R))
+// console.log("===> version1 :")
+// console.log(stackSort(R))
+console.log("===> version2 :")
+console.log(stackSort2(R))
