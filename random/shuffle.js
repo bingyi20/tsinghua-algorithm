191dc308694500ab946548d3c8adb444f7d1cfc9
ht <ht@bingyus-MBP.lan>
2022-03-19 00:49:50 +0800

    feat:    完成数组乱序shuffle算法的编写

---
+++ random/shuffle.js
@@ -0,0 +1,23 @@
+
+// 随机乱序算法
+
+// 思路
+// 从后到前，不断生成[0, j)的随机数r，然后交换 arr[r], arr[j]
+
+// Math.random() 生成[0, 1) 的随机数，
+// Math.floor(Math.random() * j) 生成[0, j)的随机数
+
+function shuffle(nums) {
+    let n = nums.length
+    // n-1, n-2, n-3, n-4
+    while(n--) {
+        let r = Math.floor(Math.random() * n)
+        let tmp = nums[r]
+        nums[r] = nums[n]
+        nums[n] = tmp
+    }
+}
+
+const nums = [1,2,3,4,5,6]
+shuffle(nums)
+console.log(nums)
