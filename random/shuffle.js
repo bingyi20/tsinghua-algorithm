b26ffdb171776643a7e567e9e8198ec8529199e8
ht <ht@bingyus-MBP.lan>
2022-03-19 01:39:58 +0800

    fix:     修复shuffle随机算法，应该谁都有机会留在最后一位

--- random/shuffle.js
+++ random/shuffle.js
@@ -2,16 +2,17 @@
 // 随机乱序算法
 
 // 思路
-// 从后到前，不断生成[0, j)的随机数r，然后交换 arr[r], arr[j]
+// 从后到前，不断生成[0, j]的随机数r，然后交换 arr[r], arr[j]
 
 // Math.random() 生成[0, 1) 的随机数，
-// Math.floor(Math.random() * j) 生成[0, j)的随机数
+// Math.floor(Math.random() * j+1) 生成[0, j]的随机数
 
 function shuffle(nums) {
     let n = nums.length
-    // n-1, n-2, n-3, n-4
-    while(n--) {
+    // n, n-1, n-2, n-3, n-4
+    while(n > 0) {
         let r = Math.floor(Math.random() * n)
+        n--
         let tmp = nums[r]
         nums[r] = nums[n]
         nums[n] = tmp
